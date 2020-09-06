bbeb337d95f91d1c8ecfd3c5485c0a701424d189
denny <denny.george90@gmail.com>
2020-09-06 17:13:28 +0530

    (feat) media endpoint done

--- src/api-server/resources/media.py
+++ src/api-server/resources/media.py
@@ -4,25 +4,23 @@ import json
 from flask import jsonify, request
 from send import add_job_to_queue
 
-post_request_parser = reqparse.RequestParser()
-post_request_parser.add_argument('file_url', type=str, help='file url is a mandatory field', required=True)
-post_request_parser.add_argument('media_type', type=str, choices=('video', 'audio', 'image'), help='The file type of media item. Acceptable values : video, image, audio', required=True)
-post_request_parser.add_argument('metadata', type=dict, help='Any metadata that you want to store in the search index. Use Judiciously')
-
 class Media(Resource):
-    def get(self):
-        return {'name': 'media'}
-    
+    def __init__(self):
+        self.post_request_parser = reqparse.RequestParser()
+        self.post_request_parser.add_argument('file_url', type=str, help='file url is a mandatory field', required=True)
+        self.post_request_parser.add_argument('media_type', type=str, choices=('video', 'audio', 'image'), help='The file type of media item. Acceptable values : video, image, audio', required=True)
+        self.post_request_parser.add_argument('metadata', type=dict, help='Any metadata that you want to store in the search index. Use Judiciously')
+
     # TODO add validation for supported mimetypes : image/jpeg, image/png, audio/mpeg, audio/wav, video/mpeg
     def post(self):
         try:
-            args = post_request_parser.parse_args(strict=True)
+            args = self.post_request_parser.parse_args(strict=True)
+            print('POST /media file_url : ', args['file_url'])
             add_job_to_queue(args)
             return 'media enqueued', 200
         
         except Exception as e:
             print('error in finding indexing media : ', e)
-            raise
-            # return 'Error indexing media : '+str(e), 500
+            return 'Error indexing media : '+str(e), 500
         
         

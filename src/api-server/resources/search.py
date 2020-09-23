7923513bf9ec406594e0636cd76522a33cd43984
Kruttika Nadig <kruttika17@gmail.com>
2020-09-23 17:58:07 +0530

    fix: results variable assigned

--- src/api-server/resources/search.py
+++ src/api-server/resources/search.py
@@ -17,10 +17,11 @@ class Search(Resource):
         parser = reqparse.RequestParser()
         parser.add_argument('file', type=werkzeug.datastructures.FileStorage, location='files')
         args = parser.parse_args()
+        print(args)
         file = args['file']
         file.save('.input/' + file.filename)
         mimetype = file.mimetype       
-
+        print(mimetype)
         if mimetype.startswith('image/'):
           print('file is image')
           media_hash, success = get_image_hash_from_local_file(file.filename)
@@ -31,6 +32,7 @@ class Search(Resource):
         elif mimetype.startswith('video/'):
           print('file is video')
           media_hash, success = get_video_hash_from_local_file(file.filename)
+          print(media_hash, success)
           videos = db['videos']
           if success == True:
             results = videos.find({"hash":media_hash})
@@ -41,14 +43,17 @@ class Search(Resource):
           if success == True:
             results = audios.find({"hash":media_hash})
         else :
-          success = false
+          success = False
+          results = []
 
         print(results, success)
 
         if success == True:
           response = []
           for result in results:
-            del result['_id'] 
+            del result['_id']
+            del result['created_at']
+            del result['updated_at']
             response.append(result)          
           return response, 200
         else:

425e854ff248c205ee8e173f5cf71ae90902e61e
denny <denny.george90@gmail.com>
2020-10-08 09:51:59 +0530

    (feat) change db connection logic to connect to remote

--- src/indexer/receive.py
+++ src/indexer/receive.py
@@ -1,3 +1,6 @@
+from helper import get_video_hash_from_s3_file, get_image_hash_from_s3_file, get_audio_hash_from_s3_file
+import logging
+from datetime import datetime
 import sys
 from os import environ
 import pika
@@ -6,10 +9,7 @@ from dotenv import load_dotenv
 import pymongo
 from pymongo import MongoClient
 load_dotenv()
-from datetime import datetime
-import logging
 
-from helper import get_video_hash_from_s3_file, get_image_hash_from_s3_file, get_audio_hash_from_s3_file
 
 credentials = pika.PlainCredentials(environ.get(
     'MQ_USERNAME'), environ.get('MQ_PASSWORD'))
@@ -19,24 +19,31 @@ channel = connection.channel()
 channel.queue_declare(queue='simple-search-index-queue', durable=True)
 channel.queue_declare(queue='simple-search-report-queue', durable=True)
 
-print('initializing db')
-mongo_url = "mongodb://db:27017"
-client = MongoClient(mongo_url)
-db = client['simple-rt-search']
+try:
+    mongo_url = "mongodb+srv://"+environ.get("SIMPLESEARCH_DB_USERNAME")+":"+environ.get(
+        "SIMPLESEARCH_DB_PASSWORD")+"@tattle-data-fkpmg.mongodb.net/test?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE"
+    cli = MongoClient(mongo_url)
+    db = cli[environ.get("SIMPLESEARCH_DB_NAME")]
+    coll = db[environ.get("SIMPLESEARCH_DB_COLLECTION")]
+except Exception as e:
+    print('Error Connecting to Mongo ', e)
+
 
 def store_hash_in_db(collection_name, doc):
-  try:
-    doc_id = db[collection_name].insert_one(doc).inserted_id
-    return doc_id
-  except Exception as e:
-    print('error storing hash in db', e)
-    raise
+    try:
+        doc_id = db[collection_name].insert_one(doc).inserted_id
+        return doc_id
+    except Exception as e:
+        print('error storing hash in db', e)
+        raise
+
 
 mimetype_collection_map = {
-  'image': 'images',
-  'video': 'videos',
-  'audio': 'audios'
+    'image': 'images',
+    'video': 'videos',
+    'audio': 'audios'
 }
+
 
 def callback(ch, method, properties, body):
     print("MESSAGE RECEIVED %r" % body)
@@ -45,15 +52,18 @@ def callback(ch, method, properties, body):
     report["source_id"] = payload["source_id"]
     report["source"] = payload["source"]
     mimetype = payload['media_type']
-    
+
     try:
         print("Generating media hash ...")
         if mimetype == 'image':
-            media_hash, success = get_image_hash_from_s3_file(payload['file_name'], payload['bucket_name'], payload['filepath_prefix'])
+            media_hash, success = get_image_hash_from_s3_file(
+                payload['file_name'], payload['bucket_name'], payload['filepath_prefix'])
         elif mimetype == 'video':
-            media_hash, success = get_video_hash_from_s3_file(payload['file_name'], payload['bucket_name'], payload['filepath_prefix'])
+            media_hash, success = get_video_hash_from_s3_file(
+                payload['file_name'], payload['bucket_name'], payload['filepath_prefix'])
         elif mimetype == 'audio':
-            media_hash, success = get_audio_hash_from_s3_file(payload['file_name'], payload['bucket_name'], payload['filepath_prefix'])
+            media_hash, success = get_audio_hash_from_s3_file(
+                payload['file_name'], payload['bucket_name'], payload['filepath_prefix'])
 
         print(media_hash, success)
         timestamp = str(datetime.utcnow())
@@ -66,18 +76,19 @@ def callback(ch, method, properties, body):
                 "updated_at": timestamp
             }
             print("Storing hash in Simple Search db ...")
-            index_id = str(store_hash_in_db(mimetype_collection_map[mimetype], document_to_be_indexed))
+            index_id = str(store_hash_in_db(
+                mimetype_collection_map[mimetype], document_to_be_indexed))
             print("Sending report to queue ...")
             report["index_timestamp"] = timestamp
             report["index_id"] = index_id
             report["status"] = "indexed"
 
             channel.basic_publish(exchange='',
-            routing_key='simple-search-report-queue',
-            properties=pika.BasicProperties(
-                content_type='application/json',
-                delivery_mode=2), # make message persistent
-            body=json.dumps(report))
+                                  routing_key='simple-search-report-queue',
+                                  properties=pika.BasicProperties(
+                                      content_type='application/json',
+                                      delivery_mode=2),  # make message persistent
+                                  body=json.dumps(report))
 
             print("Indexing success report sent to report queue")
             ch.basic_ack(delivery_tag=method.delivery_tag)
@@ -88,19 +99,18 @@ def callback(ch, method, properties, body):
         report["status"] = "failed"
         report["failure_timestamp"] = str(datetime.utcnow())
         ch.basic_publish(exchange='',
-            routing_key=properties.reply_to,
-            properties=pika.BasicProperties(
-                correlation_id=properties.correlation_id, 
-                content_type='application/json',
-                delivery_mode=2),
-            body=json.dumps(report))
+                         routing_key=properties.reply_to,
+                         properties=pika.BasicProperties(
+                             correlation_id=properties.correlation_id,
+                             content_type='application/json',
+                             delivery_mode=2),
+                         body=json.dumps(report))
         print("Indexing failure report sent to report queue")
         ch.basic_ack(delivery_tag=method.delivery_tag)
-    
 
 
-channel.basic_consume(queue='simple-search-index-queue', on_message_callback=callback)
+channel.basic_consume(queue='simple-search-index-queue',
+                      on_message_callback=callback)
 
 print(' [*] Waiting for messages. To exit press CTRL+C ')
 channel.start_consuming()
-

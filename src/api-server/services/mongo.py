07230cb3fbb7c8414ce07a5fcd9c1b486ed952eb
denny <denny.george90@gmail.com>
2020-10-16 16:55:39 +0530

    (fix) move mongourl to env file

--- src/api-server/services/mongo.py
+++ src/api-server/services/mongo.py
@@ -7,11 +7,7 @@ db = None
 
 def connect():
     try:
-        if (environ.get('APP_ENVIRONMENT') == 'development'):
-            mongo_url = "locahost:27017"
-        else:
-            mongo_url = "mongodb+srv://"+environ.get("SIMPLESEARCH_DB_USERNAME")+":"+environ.get(
-                "SIMPLESEARCH_DB_PASSWORD")+"@tattle-data-fkpmg.mongodb.net/test?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE"
+        mongo_url = environ.get('SIMPLESEARCH_MONGO_URL')
         cli = MongoClient(mongo_url)
         db = cli[environ.get("SIMPLESEARCH_DB_NAME")]
         print('Success Connecting to MongoDB')
